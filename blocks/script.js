44f1c3c95304ae2c7b1d809a49833ed928aa4032
AlbShar <sharipov.albert1991@gmail.com>
2023-11-12 22:28:08 +0500

    update animation

--- blocks/script.js
+++ blocks/script.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let lastScrollPosition =
-    window.pageYOffset || document.documentElement.scrollTop;
   const technologies = document.querySelector(".technologies");
   const engine = document.querySelector(".engine");
+  let lastScrollPosition =
+  window.scrollY || document.documentElement.scrollTop;
 
   function handleScroll() {
+    const rectTechnologies = technologies.getBoundingClientRect();
+
     const currentScrollPosition =
-      window.pageYOffset || document.documentElement.scrollTop;
+      window.scrollY || document.documentElement.scrollTop;
     const deltaY = currentScrollPosition - lastScrollPosition;
 
     if (deltaY < 0) {
       // Скролл вверх
-      if (currentScrollPosition > 542) {
-        // technologies.style = "";
+      if (engine.getBoundingClientRect().top >= rectTechnologies.height) {
+        technologies.style = "";
         engine.style = "";
       }
     } else {
@@ -21,9 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
       const rectTechnologiesBottom = rectTechnologies.bottom;
       const rectTechnologiesHeight = rectTechnologies.height;
       const marginTopCustom = rectTechnologiesHeight + 20;
-      console.log(rectTechnologies);
 
-      if (rectTechnologiesBottom <= 20 + engine.getBoundingClientRect().height) {
+      if (
+        rectTechnologiesBottom <=
+        20 + engine.getBoundingClientRect().height
+      ) {
         technologies.style.position = "fixed";
         technologies.style.zIndex = "3";
         technologies.style.top = "0";
